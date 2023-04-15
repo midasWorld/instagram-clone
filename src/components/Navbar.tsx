45d55e3885147b6b557ec9ac93d07fa880dcbfbc
midasworld <midas117@knou.ac.kr>
2023-04-15 21:34:37 +0900

    feat: 구글 로그인 추가(next-auth) - next-auth 의존성 추가 - [...nextauth].js → 구글 로그인 설정 작성 - AuthContext 생성 및 적용 - Navbar → 세션에 따라 로그인, 로그아웃 나오도록 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import SearchFillIcon from "./ui/icons/SearchFillIcon";
 import NewIcon from "./ui/icons/NewIcon";
 import NewFillIcon from "./ui/icons/NewFillIcon";
 import ColorButton from "./ui/ColorButton";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const menu = [
   {
@@ -30,6 +31,7 @@ const menu = [
 
 export default function Navbar() {
   const pathname = usePathname();
+  const { data: session } = useSession();
   return (
     <div className="flex justify-between items-center px-6">
       <Link href="/">
@@ -44,7 +46,11 @@ export default function Navbar() {
               </Link>
             </li>
           ))}
-          <ColorButton text="Sign in" onClick={() => {}} />
+          {session ? (
+            <ColorButton text="Sign out" onClick={signOut} />
+          ) : (
+            <ColorButton text="Sign in" onClick={signIn} />
+          )}
         </ul>
       </nav>
     </div>
