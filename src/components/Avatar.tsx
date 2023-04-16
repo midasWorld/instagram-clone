16766be2d1d38f0f531817925f403b052f6626ff
midasworld <midas117@knou.ac.kr>
2023-04-17 00:04:02 +0900

    fix: Avatar - image props → 타입에 null 추가 (Null 허용으로 변경)

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-type Props = { image?: string };
+type Props = { image?: string | null };
 export default function Avatar({ image }: Props) {
   return (
     <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
