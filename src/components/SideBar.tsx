9d137c0441b930050be12f23b5a72b1aa6335981
midasworld <midas117@knou.ac.kr>
2023-04-17 11:08:47 +0900

    feat: 사이드바(회원 정보, Footer 정보) 구현 - `user` Model 정의 → next-auth session user 에 적용 - Avatar 컴포넌트 → size, highlight 설정 추가 - Sidebar 컴포넌트 구현

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export default function SideBar() {
-  return <p>SideBar</p>;
+import { AuthUser } from "@/model/user";
+import Avatar from "./Avatar";
+
+type Props = {
+  user: AuthUser;
+};
+export default function SideBar({ user: { username, name, image } }: Props) {
+  return (
+    <>
+      <div className="flex items-center">
+        <Avatar image={image} />
+        <div className="ml-4">
+          <p className="font-bold">{username}</p>
+          <p className="text-lg text-neutral-500 leading-3">{name}</p>
+        </div>
+      </div>
+      <div>
+        <p className="text-sm text-neutral-500 mt-8">
+          About · Help · Press · API · Jobs · Privacy · Terms · Location ·
+          Language
+        </p>
+      </div>
+      <div>
+        <p className="text-neutral-500 font-bold text-sm mt-8">
+          @Copyright INSTAGRAM from METAL
+        </p>
+      </div>
+    </>
+  );
 }
