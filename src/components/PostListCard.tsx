f3279ec0e1b56680e58ae44ff3c752dabb4ce4f8
midasworld <midas117@knou.ac.kr>
2023-04-18 04:20:27 +0900

    feat: 게시글 상세보기 UI 구현 - PostUserAvatar 컴포넌트 생성(게시글 상단 부분(글쓴이) 분리) - FollowingBar 컴포넌트 CSS 수정(팝업창 보다 Carousel 이 위로 와있어 z-index 수정)

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./PostModal";
 import PostDetail from "./PostDetail";
+import PostUserAvatar from "./PostUserAvatar";
 
 type Props = {
   post: SimplePost;
@@ -20,10 +21,7 @@ export default function PostListCard({ post, priority = false }: Props) {
   const [openModal, setOpenModal] = useState(false);
   return (
     <article className="rounded-lg shadow-md border border-gray-200">
-      <div className="flex items-center p-2">
-        <Avatar image={userImage} size="medium" highlight />
-        <span className="text-gray-900 font-bold ml-2">{username}</span>
-      </div>
+      <PostUserAvatar image={userImage} username={username} />
       <Image
         className="w-full object-cover aspect-square"
         src={image}
