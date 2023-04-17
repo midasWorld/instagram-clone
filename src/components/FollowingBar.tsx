802123191ef1e84eeaa24f8b453d7063fa77cee0
midasworld <midas117@knou.ac.kr>
2023-04-17 17:28:53 +0900

    feat: FollowingBar 구현(with swr, react-spinners) - SWR Context 생성 - FollowingBar 컴포넌트 구현(기능 & CSS) - `react-spinners` 활용한 로딩 추가 - Avatar 컴포넌트 → 이미지가 찌그러지는 현상 해결을 위해 `object-cover` 속성 추가

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
+import useSWR from "swr";
+import Avatar from "./Avatar";
+
 export default function FollowingBar() {
-  return <p>FollowingBar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+  const users = data?.following;
+  return (
+    <section className="w-full flex justify-center items-center p-4 shadow-md shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{"You don't have following"}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul className="w-full flex gap-2">
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link
+                className="flex flex-col items-center w-20"
+                href={`/user/${username}`}
+              >
+                <Avatar image={image} highlight />
+                <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                  {username}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
