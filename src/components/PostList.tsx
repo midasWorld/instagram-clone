5c8338f2995ff2d123ff757420768a82a4e7f88e
midasworld <midas117@knou.ac.kr>
2023-04-17 23:53:50 +0900

    feat: 팔로잉 게시물 조회 기능 구현 - sanity: Image URL 빌드 함수 추가 - post 모델 타입 생성

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { SimplePost } from "@/model/post";
+import useSWR from "swr";
+
 export default function PostList() {
-  return <p>PostList</p>;
+  const {
+    data: posts,
+    isLoading: loading,
+    error,
+  } = useSWR<SimplePost[]>("/api/posts");
+  console.log(posts);
+  return (
+    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+  );
 }
