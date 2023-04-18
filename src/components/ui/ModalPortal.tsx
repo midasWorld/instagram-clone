2c8c9534683090579fd04aec0c3f9e6b99a3d1b8
Midas <midas117@knou.ac.kr>
2023-04-18 16:43:09 +0900

    [Feat] 게시글 상세 페이지 구현 (#5)
    * feat: 기본적인 Modal 구현(using React Portals)
    
    * feat: 게시글 상세보기에서 댓글 정보 가져오기 기능(API) 구현
    
    * feat: 게시글 상세보기 UI 구현
    - PostUserAvatar 컴포넌트 생성(게시글 상단 부분(글쓴이) 분리)
    - FollowingBar 컴포넌트 CSS 수정(팝업창 보다 Carousel 이 위로 와있어 z-index 수정)
    
    * feat: 전체 CSS 수정 (로그인 페이지 버튼 문제도 같이 해결)
    - 로그인 페이지 버튼 배경만 높이만큼 길어졌던 원인이 `layout` → main → `min-h-full` ... 😓

---
+++ src/components/ui/ModalPortal.tsx
@@ -0,0 +1,14 @@
+import ReactDom from "react-dom";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ModalPortal({ children }: Props) {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  const node = document.getElementById("portal") as Element;
+  return ReactDom.createPortal(children, node);
+}
