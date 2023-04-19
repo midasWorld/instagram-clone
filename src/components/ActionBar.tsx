af15e92ea43f98849a59791b21078f61fe89de68
midasworld <midas117@knou.ac.kr>
2023-04-19 22:31:53 +0900

    feat: 게시글 목록 → 댓글 정보 UI & 기능 구현 - ActionBar 컴포넌트: text 부분 분리   (PostListCard 에서만 쓰이니 여기서 주입받도록!)

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -10,9 +10,10 @@ import HeartIcon from "./ui/icons/HeartIcon";
 
 type Props = {
   post: SimplePost;
+  children: React.ReactNode;
 };
 
-export default function ActionBar({ post }: Props) {
+export default function ActionBar({ post, children }: Props) {
   const { id, likes, username, text, createdAt } = post;
   const { user, setBookmark } = useMe();
   const { setLike } = usePosts();
@@ -48,12 +49,7 @@ export default function ActionBar({ post }: Props) {
         <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${
           likes?.length > 1 ? "likes" : "like"
         }`}</p>
-        {text && (
-          <p>
-            <span className="font-bold mr-1">{username}</span>
-            {text}
-          </p>
-        )}
+        {children}
         <p className="text-xs text-neutral-500 uppercase my-2">
           {parseDate(createdAt)}
         </p>
