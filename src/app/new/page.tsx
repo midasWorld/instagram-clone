692e075c8e809ddc1e37e2285cb1f9f4ad26bd76
midasworld <midas117@knou.ac.kr>
2023-04-20 12:01:48 +0900

    feat: 게시글 작성 페이지 UI 구현 - Files 아이콘 생성

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,19 @@
-export default function NewPage() {
-  return <h1>New Page</h1>;
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+
+  return <NewPost user={session.user} />;
 }
